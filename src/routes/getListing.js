5643f0807a2a82f739ef036881011fe64395bbd3
Umme-hanii <ummehanimohammed9@gmail.com>
2023-06-12 00:35:58 +0530

    Install hapi Boom to handle errors

--- src/routes/getListing.js
+++ src/routes/getListing.js
@@ -1,3 +1,4 @@
+import Boom from '@hapi/boom'
 import { fakeListings } from './fake-data.js'
 
 export const getListingRoute = {
@@ -5,6 +6,8 @@ export const getListingRoute = {
   path: '/api/listing/{id}',
   handler: (req, h) => {
     const id = req.params.id
-    return fakeListings.find(listing => listing.id === id)
+    const listing = fakeListings.find(listing => listing.id === id)
+    if(!listing) throw Boom.notFound(`Listing with id ${id} is not found`)
+    return listing
   }
 }
